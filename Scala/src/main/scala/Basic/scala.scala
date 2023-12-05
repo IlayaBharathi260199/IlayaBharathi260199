ed030f0d7e9f2f7d3410a092168c82a1db269f94
IlayaBharathi260199 <ilayabharathi260199@gmail.com>
2023-12-06 00:24:35 +0530

    AC V2

--- Scala/src/main/scala/Basic/scala.scala
+++ Scala/src/main/scala/Basic/scala.scala
@@ -20,11 +20,27 @@ object scala {
     println
     println(mylist)
 
+
     // using LAMBDA(=>) we can achive this
     val modlist = mylist.map(x => x + 20)
     println
     println(modlist)
 
+    //String concat
+    val myStringlist = List("ilayabha","bharathi","ilaya")
+    val modlist1 = myStringlist.map(x => x + " you are good")
+    println(myStringlist)
+    println
+    println(modlist1)
+    println
+
+    //Replace with LAMBDA(=>)
+    val mylistReplace = myStringlist.map(x => x.replace("ilaya","bharathi"))
+    println(mylistReplace)
+
+    // want only data contains "ilaya"
+    val mylistcont = myStringlist.filter(x => x.contains("ilaya"))
+    println(mylistcont)
 
 
   }
