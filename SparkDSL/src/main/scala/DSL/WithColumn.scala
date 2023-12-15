33a27d14d60c78aea4c85b5370047d3d1f77aa96
IlayaBharathi260199 <ilayabharathi260199@gmail.com>
2023-12-15 23:26:43 +0530

    231215 URL_Read

--- SparkDSL/src/main/scala/DSL/WithColumn.scala
+++ SparkDSL/src/main/scala/DSL/WithColumn.scala
@@ -18,10 +18,22 @@ object WithColumn {
     val csv = spark.read.option("header", "true").csv("/home/ubuntu/IdeaProjects/ilaya/IlayaBharathi260199/Scala/files/dt.csv")
     csv.show(false)
 
-    csv.withColumn("tdate",expr("split(tdate,'-')[2]"))
-      .withColumnRenamed("tdate","year")
-      .withColumn("Status",expr("case when spendby = 'cash' then 1 else 0 end")).show(false)
+    csv.withColumn("tdate", expr("split(tdate,'-')[2]"))
+      .withColumnRenamed("tdate", "year")
+      .withColumn("Status", expr("case when spendby = 'cash' then 1 else 0 end")).show(false)
 
+
+    // to remove "Numerical" values from data pattern = "\\d"(common)
+    val num = csv.withColumn("category", regexp_replace(col("category"), "\\d", ""))
+    println()
+    println("=====Numbers Removed=====")
+    num.show(false)
+
+    // To remove Special characters from data pattern = "[^a-zA-Z ]"(common)
+    val speChar = num.withColumn("product", regexp_replace(col("product"), "[^a-zA-Z ]", ""))
+    println()
+    println("=====Special characters Removed=====")
+    speChar.show(false)
 
 
   }
