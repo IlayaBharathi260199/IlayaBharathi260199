b91793b90f8253ac1c9651599796eeec77e65ac0
IlayaBharathi260199 <ilayabharathi260199@gmail.com>
2023-12-25 20:52:23 +0530

    232523_SparkkSubmit_UP

--- Scripts/spark-submit/SparkSubmit.sh
+++ Scripts/spark-submit/SparkSubmit.sh
@@ -2,7 +2,9 @@
 
 
 # Script is suitable for local run
-# For cluster run change "--deploy-mode cluster",-"-master YARN" in SPARK_SUBMIT_OPTIONS
+# For cluster run
+# 1.change "--deploy-mode cluster",-"-master YARN" in SPARK_SUBMIT_OPTIONS
+# 2.Add resources as per data (Static(Manual) or Dynamic(Auto))
 
 # Set the Spark home directory
 SPARK_HOME="/home/ubuntu/Downloads/spark-3.4.1-bin-hadoop3/"
@@ -13,13 +15,13 @@ SPARK_HOME="/home/ubuntu/Downloads/spark-3.4.1-bin-hadoop3/"
 APP_JAR="/home/ubuntu/IdeaProjects/ilaya/IlayaBharathi260199/SparkDSL/target/scala-2.12/sparkdsl_2.12-1.0.jar"
 
 #   =====Change MAIN_CLASS path According to Your Requirement=====
-MAIN_CLASS="dsl.Joins"
+MAIN_CLASS="dsl.Word_count"
 
 #   =====Change JOB_NAME(optional) path According to Your Requirement=====
 JOB_NAME="SparkJob"
 
 # Set other Spark-submit options
-SPARK_SUBMIT_OPTIONS="--master local --deploy-mode client"
+SPARK_SUBMIT_OPTIONS="--master local --deploy-mode client --num-executors 2 --executor-memory 2g"
 
 # Run spark-submit command
 $SPARK_HOME/bin/spark-submit --name $JOB_NAME $SPARK_SUBMIT_OPTIONS --class $MAIN_CLASS $APP_JAR
